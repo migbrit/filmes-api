052b3e4cd701274f01b25baf881780847c106c8c
Miguel Brito <miguelfelipe_steam@outlook.com>
2023-06-03 15:22:54 -0300

    feat: initial endpoints

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,33 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("Filme")]
+    public class FilmeController : ControllerBase
+    {
+        List<Filme> filmes = new List<Filme>();
+
+        int id = 0;
+
+        [Route("Add")]
+        [HttpPost]
+        public void AddFilme(Filme filme)
+        {
+            id++;
+            filme.Id = id;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Genero);
+            Console.WriteLine(filme.Duracao);
+        }
+
+        [Route("Get")]
+        [HttpGet("{id}")]
+        public Filme GetFilmeById(int id)
+        {
+            return filmes.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Titulo { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600)]
+        public int Duracao { get; set; }
+    }
+}
