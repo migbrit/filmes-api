caea045f6652559890367b66751d8f9d6502282d
Miguel Brito <miguelfelipe_steam@outlook.com>
2023-06-03 16:29:49 -0300

    feat: adding getFilmes endpoint with pagination and memory control

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -3,31 +3,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers
 {
+
     [ApiController]
-    [Route("Filme")]
+    [Route("api")]
     public class FilmeController : ControllerBase
     {
-        List<Filme> filmes = new List<Filme>();
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
-        int id = 0;
-
-        [Route("Add")]
+        [Route("addfilme")]
         [HttpPost]
         public void AddFilme(Filme filme)
         {
             id++;
             filme.Id = id;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Genero);
-            Console.WriteLine(filme.Duracao);
         }
 
-        [Route("Get")]
-        [HttpGet("{id}")]
-        public Filme GetFilmeById(int id)
+        [Route("getfilmes")]
+        [HttpGet]
+        public List<Filme> GetFilmes(int skip, int take)
         {
-            return filmes.Where(x => x.Id == id).FirstOrDefault();
+            return filmes.Skip(skip).Take(take).ToList();
         }
+
+        [Route("getfilme")]
+        [HttpGet]
+        public Filme? GetFilmeById(int id)
+        {
+            Filme? filme = filmes.FirstOrDefault(x => x.Id == id);
+            return filme;
+        }
+
     }
 }
